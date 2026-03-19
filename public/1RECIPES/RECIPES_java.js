4d8c5c7e80f0f6297608768cb5b7e1feef4ed185
BettinaFernadez <b2029bfbfernandez@pshs.edu.ph>
2026-03-19 11:52:30 +0800

    sijfsijfosijdfj

--- public/1RECIPES/RECIPES_java.js
+++ public/1RECIPES/RECIPES_java.js
@@ -1,20 +1,41 @@
 // Add all your recipe data here!
 const recipeData = [
     {
+        title: "Rice and Vegetables",
+        desc: "",
+        link: "../rice_veggies_recipes/rice_veggies.html"
+    },
+    {
         title: "Beef Recipes",
         desc: "Hearty and savory dishes for the meat lover. From classic stews to sizzling steaks, explore the rich flavors of beef!",
-        link: "/beef_recipes/beef.html",
+        link: "../beef_recipes/beef.html",
     },
     {
         title: "Pork Recipes",
         desc: "Versatile and delicious. Discover sweet and savory pork recipes that are 'Even Better Than Mama's!'",
-        link: "/pork_recipes/pork.html",
+        link: "../pork_recipes/pork.html",
     },
     {
         title: "Desserts",
         desc: "Sweet, mouthwatering treats to end your meal. From cakes to puddings, Phoebe's sweet tooth has you covered!",
-        link: "/desserts_recipes/desserts.html",
+        link: "../desserts_recipes/desserts.html",
+    },
+    {
+        title: "Chicken Recipes",
+        desc: "",
+        link: "../chicken_recipes/chicken.html"
+    },
+    {
+       title: "Beverages",
+       desc: "",
+       link:  "../drink_recipes/drinks.html"
+    },
+    {
+        title: "Pasta Recipes",
+        desc: "",
+        link: "../pasta_recipes/pasta_noodles.html"
     }
+
 ];
 
 let currentIndex = 0;
